de16d1d7985168312eab7508259f06d180da8131
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2016-01-22 18:57:36 -0500

    Hari Edited Person1.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Hari
  *  @version 1.1
  */
 public class Person1 {
@@ -31,7 +31,11 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+        String newString = "";
+        for (int i = 0; i < newString.length(); i++) {
+            newString = newString + input.charAt((i+2) % input.length());
+        }
+	  return newString;
 	}
 	
 	/**

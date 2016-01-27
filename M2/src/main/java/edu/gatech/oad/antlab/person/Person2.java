b6726b95b0e38bd4a5651daebc9d81dcc99be271
ritzjwa <ritzee1T>
2016-01-26 19:43:10 -0500

    Edited code for person 2

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,11 +1,14 @@
 package edu.gatech.oad.antlab.person;
-
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+import java.lang.String;
 /**
  *  A simple class for person 2
  *  returns their name and a
  *  modified string 
  *
- * @author Bob
+ * @author Ritika Ravichandra
  * @version 1.1
  */
 public class Person2 {
@@ -29,10 +32,27 @@ public class Person2 {
 	 * @param input the string to be modified
 	 * @return the modified string
 	 */
-	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
-	}
+    private static String calc(String input) {
+        List<String> shuffle = new ArrayList();
+        for(int i = 0; i < input.length(); i++){
+            String temp = Character.toString(input.charAt(i));
+            shuffle.add(temp);
+        }
+        System.out.println(shuffle.size());
+
+        Random rand = new Random();
+        String newString = "";
+        while(!shuffle.isEmpty()) {
+            int index = rand.nextInt(shuffle.size());
+            String letter = shuffle.get(index);
+            newString = newString + letter;
+            System.out.println(index);
+            shuffle.remove(index);
+        }
+        return newString;
+
+    }
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
